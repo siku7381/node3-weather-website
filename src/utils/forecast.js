e5c2004c9feb088fca942be36e1e73be0a538e6d
Your Name <you@example.com>
2020-04-30 16:24:18 +0530

    Add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (lat,long,callback) => {
         else if (body.error) {
             callback('Unbale to find the location. Try another search.',undefined);
         } else {
-            callback(undefined, `${body.current.weather_descriptions[0]} . It is currently ${body.current.temperature} degrees out.It feels like ${body.current.feelslike} degrees out.`);
+            callback(undefined, `${body.current.weather_descriptions[0]} . It is currently ${body.current.temperature} degrees out.It feels like ${body.current.feelslike} degrees out. Now it's ${body.current.humidity}% humidity outside.`);
         }
     })
 }
